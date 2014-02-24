35fe42d4a952c9460eef62dc2f57774833f7f419
Dirk Grappendorf <dirk@grappendorf.net>
2014-02-25 00:18:49 +0100

    Updated the specs because of the new Xbee constructor Updated the header of all source code files

--- spec/xbee_spec.rb
+++ spec/xbee_spec.rb
@@ -2,19 +2,9 @@
 
 This file is part of the xbee-ruby gem.
 
-Copyright 2013 Dirk Grappendorf, www.grappendorf.net
+Copyright 2013-2014 Dirk Grappendorf, www.grappendorf.net
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Licensed under the The MIT License (MIT)
 
 =end
 
@@ -25,7 +15,7 @@ module XBeeRuby
 	describe XBee do
 
 		let!(:serial) { double('SerialPort').as_null_object }
-		let!(:xbee) { XBee.new '/dev/ttyS0', 57600 }
+		let!(:xbee) { XBee.new port: '/dev/ttyS0', rate: 57600 }
 
 		before { allow(SerialPort).to receive(:create).and_return serial }
 
